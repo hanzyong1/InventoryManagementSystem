ac80553bd7dd367066b14ae0c3fb360520d4a432
zhiyong <zhiyong@codongenomics.com>
2023-11-29 10:00:29 +0800

    Add project files.

---
+++ InventoryManagementSystem.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace InventoryManagementSystem.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ InventoryManagementSystem.Data/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace InventoryManagementSystem.Data
+{
+    public class Class1
+    {
+
+    }
+}
