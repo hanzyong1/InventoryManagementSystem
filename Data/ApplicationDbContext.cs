ed05be80da6ac4ec46e5e61f9b3d48112c078f15
zhiyong <zhiyong@codongenomics.com>
2024-09-14 18:21:32 +0800

    add initial migration for Product

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using InventoryManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagementSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
