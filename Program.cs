4ddf63b325a7491b6d87f93868abd9b7cd53bfd0
zhiyong <zhiyong@codongenomics.com>
2024-09-24 14:03:37 +0800

    add dependency injection for ProductRepository and CategoryRepository

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using InventoryManagementSystem.Data;
+using InventoryManagementSystem.Data.Repositories;
+using InventoryManagementSystem.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +14,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 var app = builder.Build();
 
