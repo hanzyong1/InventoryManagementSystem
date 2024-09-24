53f0aaae443bad681b71b65b663f0abcf240f137
zhiyong <zhiyong@codongenomics.com>
2024-09-24 14:03:03 +0800

    add GetAll to ProductService

---
+++ Services/ProductService.cs
@@ -0,0 +1,47 @@
+﻿using InventoryManagementSystem.Data.Repositories;
+using InventoryManagementSystem.Dtos.CategoryDto;
+using InventoryManagementSystem.Dtos.ProductDto;
+
+namespace InventoryManagementSystem.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public ProductService(
+            IProductRepository productRepository,
+            ICategoryRepository categoryRepository
+            )
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+        public async Task<List<GetProductDto>> GetAll()
+        {
+            var products = await _productRepository.GetAll();
+            var categoryList = await _categoryRepository.GetAll();
+
+            var productDtos = products.Select(product =>
+            {
+                var category = categoryList.FirstOrDefault(e => e.Id == product.CategoryId);
+
+                return new GetProductDto
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    Description = product.Description,
+                    Category = new GetCategoryDto
+                    {
+                        Id = category.Id,
+                        Name = category.Name,
+                    },
+                    Quantity = product.Quantity,
+                    Price = product.Price,
+                };
+            }).ToList();
+
+            return productDtos;
+        }
+    }
+}
