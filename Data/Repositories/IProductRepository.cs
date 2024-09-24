5dc590d5bd2c4ec4bf8fc25ceeea3f2d4792d74a
zhiyong <zhiyong@codongenomics.com>
2024-09-24 13:58:29 +0800

    add GetAll to IProductRepository

---
+++ Data/Repositories/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using InventoryManagementSystem.Models;
+
+namespace InventoryManagementSystem.Data.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> GetAll();
+    }
+}
