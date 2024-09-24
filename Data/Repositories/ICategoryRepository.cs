ca495d1d251036ae5954e7b972c60c353f82aa92
zhiyong <zhiyong@codongenomics.com>
2024-09-24 13:59:22 +0800

    add GetAll to ICategoryRepository

---
+++ Data/Repositories/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using InventoryManagementSystem.Models;
+
+namespace InventoryManagementSystem.Data.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<List<Category>> GetAll();
+    }
+}
