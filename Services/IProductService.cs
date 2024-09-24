e4a90e3fb1086dfee64658e29430ba690750e369
zhiyong <zhiyong@codongenomics.com>
2024-09-24 14:02:35 +0800

    add GetAll to IProductService

---
+++ Services/IProductService.cs
@@ -0,0 +1,9 @@
+﻿using InventoryManagementSystem.Dtos.ProductDto;
+
+namespace InventoryManagementSystem.Services
+{
+    public interface IProductService
+    {
+        Task<List<GetProductDto>> GetAll();
+    }
+}
