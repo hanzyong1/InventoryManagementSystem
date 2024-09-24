e4f7480ae4aed9a6327011c2ffb98a1b5140cfc4
zhiyong <zhiyong@codongenomics.com>
2024-09-24 13:59:34 +0800

    add GetAll to CategoryRepository

---
+++ Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using InventoryManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagementSystem.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Category>> GetAll()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+    }
+}
