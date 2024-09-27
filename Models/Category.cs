7da773e7e7ee3e6562554212c4edc5f318996700
zhiyong <zhiyong@codongenomics.com>
2024-09-27 10:44:47 +0800

    add initial seed data for Category

--- Models/Category.cs
+++ Models/Category.cs
@@ -2,6 +2,15 @@
 {
     public class Category
     {
+        public Category()
+        {
+            
+        }
+        public Category(string name)
+        {
+            Name = name;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public ICollection<Product> Products { get; set; } = new List<Product>();
