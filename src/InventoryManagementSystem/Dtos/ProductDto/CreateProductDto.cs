64ddd10bd1322fe19309e1992995ce44c8eec5db
zhiyong <zhiyong@codongenomics.com>
2024-10-17 16:45:49 +0800

    add CreateProductDto

---
+++ src/InventoryManagementSystem/Dtos/ProductDto/CreateProductDto.cs
@@ -0,0 +1,13 @@
+﻿using InventoryManagementSystem.Dtos.CategoryDto;
+
+namespace InventoryManagementSystem.Dtos.ProductDto
+{
+    public class CreateProductDto
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public int CategoryId { get; set; }
+        public int? Quantity { get; set; }
+        public double? Price { get; set; }
+    }
+}
