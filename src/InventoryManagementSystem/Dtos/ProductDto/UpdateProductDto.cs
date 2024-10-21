ba75b7b1bd3149d9bec4b7688f20335ec1df56c2
zhiyong <zhiyong@codongenomics.com>
2024-10-21 21:26:31 +0800

    add UpdateProductDto

---
+++ src/InventoryManagementSystem/Dtos/ProductDto/UpdateProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace InventoryManagementSystem.Dtos.ProductDto
+{
+    public class UpdateProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public int CategoryId { get; set; }
+        public int? Quantity { get; set; }
+        public double? Price { get; set; }
+    }
+}
