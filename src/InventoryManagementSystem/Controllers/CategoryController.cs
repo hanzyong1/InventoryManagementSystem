447d9bcbc2f5eec5254fa40b79bfd96bc34e9f0d
zhiyong <zhiyong@codongenomics.com>
2024-10-22 21:17:27 +0800

    add Get endpoint to CategoryController

---
+++ src/InventoryManagementSystem/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using InventoryManagementSystem.Dtos.CategoryDto;
+using InventoryManagementSystem.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("Get/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Get(int id)
+        {
+            var category = await _categoryService.Get(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
+    }
+}
