27b7e47577d893ebc49e4cd893cebb608d809a10
zhiyong <zhiyong@codongenomics.com>
2024-10-31 21:27:38 +0800

    add UpdateCategoryDto

---
+++ src/InventoryManagementSystem/Dtos/CategoryDto/UpdateCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace InventoryManagementSystem.Dtos.CategoryDto
+{
+    public class UpdateCategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
