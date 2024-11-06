b8bc2415781175046bca2c0c5ea129d1c3cf89a4
zhiyong <zhiyong@codongenomics.com>
2024-11-06 21:14:49 +0800

    add GetStockDto

---
+++ src/InventoryManagementSystem/Dtos/StockDto/GetStockDto.cs
@@ -0,0 +1,13 @@
+﻿using InventoryManagementSystem.Dtos.ProductDto;
+using InventoryManagementSystem.Dtos.WarehouseDto;
+
+namespace InventoryManagementSystem.Dtos.StockDto
+{
+    public class GetStockDto
+    {
+        public int Id { get; set; }
+        public GetWarehouseDto Warehouse { get; set; }
+        public GetProductDto Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
