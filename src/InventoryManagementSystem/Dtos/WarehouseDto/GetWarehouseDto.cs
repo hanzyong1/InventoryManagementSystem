6af08992ea28e24f207d1f23f3164be2c6fe1f47
zhiyong <zhiyong@codongenomics.com>
2024-11-06 21:14:58 +0800

    add GetWarehouseDto

---
+++ src/InventoryManagementSystem/Dtos/WarehouseDto/GetWarehouseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace InventoryManagementSystem.Dtos.WarehouseDto
+{
+    public class GetWarehouseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
