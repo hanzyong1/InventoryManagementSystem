6cb55ba298318ec8b6d462691671fed99a125200
zhiyong <zhiyong@codongenomics.com>
2024-11-06 21:16:06 +0800

    add GetAllStocksOfProduct endpoint to StockController

---
+++ src/InventoryManagementSystem/Controllers/StockController.cs
@@ -0,0 +1,39 @@
+﻿using InventoryManagementSystem.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly IStockService _stockService;
+        private readonly IProductService _productService;
+
+        public StockController(
+            IStockService stockService,
+            IProductService productService
+            )
+        {
+            _stockService = stockService;
+            _productService = productService;
+        }
+
+        [HttpGet("GetAllStocksOfProduct/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetAllStocksOfProduct(int id)
+        {
+            var product = await _productService.Get(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            var stocks = await _stockService.GetAllStocksOfProduct(id);
+
+            return Ok(stocks);
+        }
+    }
+}
