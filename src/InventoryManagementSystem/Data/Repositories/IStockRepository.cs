71133f1eb72404ebf9451bd496138114db16ea5a
zhiyong <zhiyong@codongenomics.com>
2024-11-06 21:14:18 +0800

    add Get, GetAll, GetAllByProductId to StockRepository

---
+++ src/InventoryManagementSystem/Data/Repositories/IStockRepository.cs
@@ -0,0 +1,11 @@
+﻿using InventoryManagementSystem.Models;
+
+namespace InventoryManagementSystem.Data.Repositories
+{
+    public interface IStockRepository
+    {
+        Task<Stock?> Get(int id);
+        Task<List<Stock>> GetAllByProductId(int productId);
+        Task<List<Stock>> GetAll();
+    }
+}
---
+++ src/InventoryManagementSystem/Data/Repositories/StockRepository.cs
@@ -0,0 +1,33 @@
+﻿using InventoryManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagementSystem.Data.Repositories
+{
+    public class StockRepository : IStockRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StockRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Stock?> Get(int id)
+        {
+            return await _context.Stocks.FirstOrDefaultAsync(e => e.Id == id);
+        }
+
+        public async Task<List<Stock>> GetAllByProductId(int productId)
+        {
+            return await _context.Stocks
+                .Include(e => e.Warehouse)
+                .Where(e => e.ProductId == productId)
+                .ToListAsync();
+        }
+
+        public async Task<List<Stock>> GetAll()
+        {
+            return await _context.Stocks.ToListAsync();
+        }
+    }
+}
