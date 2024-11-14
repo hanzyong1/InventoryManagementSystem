9857f3c38a6eed582a3d7d8389817192b25efeb6
zhiyong <zhiyong@codongenomics.com>
2024-11-14 21:03:24 +0800

    add GetStockAmountDto

---
+++ src/InventoryManagementSystem/Dtos/StockDto/GetStockAmountDto.cs
@@ -0,0 +1,10 @@
+﻿using InventoryManagementSystem.Dtos.ProductDto;
+
+namespace InventoryManagementSystem.Dtos.StockDto
+{
+    public class GetStockAmountDto
+    {
+        public GetProductDto Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
